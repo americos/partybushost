1aec2582247b025ebf8e1e35f93fb5422b61b450
Americo Savinon <americos@gmail.com>
2011-07-14 14:35:50 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partybushost::Application.config.secret_token = 'fc56b2caa20873438fc0a22be3e5fc369599e512476e3f1e67ecc9075ce44ca693a09d35f24fa856aebffb15b91ee03f2da45bd8e56f90d94e026de72014217b'
